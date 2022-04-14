85a99810037bbf3b5de7bdf6271fd1d8bf0299db
joshboye <joshboye8@gmail.com>
2022-04-14 18:48:01 +0530

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React from "react"
+import './app.css'
+import Table from './table.js'
+
+const App = () => {
+    return(<div className="contain"><Table /></div>)
+}
+
+export default App
---
+++ src/components/table.js
@@ -0,0 +1,112 @@
+import React from "react";
+import './table.css'
+
+const Table = () =>{
+    return(
+        <div className="container">
+            <table className="tablecontain">
+                <tr className="row1">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row2">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row3">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row1">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row2">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row3">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr><tr className="row1">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row2">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+                <tr className="row3">
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                </tr>
+        </table>
+        </div>
+    )
+
+}
+
+export default Table
